522abbe91558372d94c5ead289d786583280bdd9
zr <hanliwoyaoai@163.com>
2017-10-25 16:25:23 +0800

    添加日志

--- src/com/bonc/Dbutils.java
+++ src/com/bonc/Dbutils.java
@@ -1,8 +1,25 @@
 package com.bonc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Dbutils {
     private static String DRIVER_CLASS = "";
     private static String DB_URL = "";
     private static String DB_USER = "";
     private static String DB_PASS = "";
+
+    private static Dbutils dbutils;
+    private static Connection conn;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER_CLASS);
+        } catch (Exception e) {
+
+        }
+
+
+        return null;
+    }
 }
