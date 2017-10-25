591f7c27d766fba93198350399acfd39a9e56934
zr <hanliwoyaoai@163.com>
2017-10-25 16:05:48 +0800

    添加日志

---
+++ src/com/bonc/Dbutils.java
@@ -0,0 +1,8 @@
+package com.bonc;
+
+public class Dbutils {
+    private static String DRIVER_CLASS = "";
+    private static String DB_URL = "";
+    private static String DB_USER = "";
+    private static String DB_PASS = "";
+}
